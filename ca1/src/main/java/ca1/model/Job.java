fd7e1f6482c4bad8e640b438276cbaec8e23a9f7
Ricardo <d00262961@student.dkit.ie>
2024-10-05 18:50:30 +0100

    Created Job class inside model sub-package.

---
+++ ca1/src/main/java/ca1/model/Job.java
@@ -0,0 +1,66 @@
+package ca1.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Job {
+    private int id;
+    private String owner;
+    private String description;
+    private LocalDateTime created;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    @Override
+    public String toString() {
+        return "Job{" +
+                "id=" + id +
+                ", owner='" + owner + '\'' +
+                ", description='" + description + '\'' +
+                ", created=" + created +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Job job = (Job) o;
+        return id == job.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
